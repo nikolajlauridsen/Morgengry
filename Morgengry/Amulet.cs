18a195f216fd54037f1ba4da5d559aae5b327e9a
Molex <simplesleeptimer@gmail.com>
2018-10-24 11:46:26 +0200

    Add Amulet (And inital commit)

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry {
+    class Amulet {
+        public string ItemId;
+        public string Design;
+        public enum Level { low, medium, high}
+        public Level Quality;
+
+        Amulet(String itemId) {
+            ItemId = itemId;
+        }
+
+        Amulet(String itemId, Level quality) {
+            ItemId = itemId;
+            Quality = quality;
+        }
+
+        Amulet(String itemId, Level quality, String design) {
+            ItemId = itemId;
+            Quality = quality;
+            Design = design;
+        }
+    }
+}
