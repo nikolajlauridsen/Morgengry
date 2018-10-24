c4a9cd6827ba342876a1e6db3653b771d9f5d5f1
Molex <simplesleeptimer@gmail.com>
2018-10-24 12:20:58 +0200

    Be smart, use pattern from book for construtors Mind=blown

--- Morgengry/Amulet.cs
+++ Morgengry/Amulet.cs
@@ -12,20 +12,19 @@ namespace Morgengry {
         public string Design; 
         public Level Quality;
 
-        public Amulet(String itemId) {
-            ItemId = itemId;
-        }
-
-        public Amulet(String itemId, Level quality) {
-            ItemId = itemId;
-            Quality = quality;
-        }
-
         public Amulet(String itemId, Level quality, String design) {
             ItemId = itemId;
             Quality = quality;
             Design = design;
         }
+
+        public Amulet(String itemId): this(itemId, Level.medium, null) {
+        }
+
+        public Amulet(String itemId, Level quality): this(itemId, quality, null) {
+        }
+
+
 
         public override string ToString() {
             return String.Format("ItemId: {0}, Quality: {1}, Design: {2}", ItemId, Quality, Design);
--- Morgengry/Book.cs
+++ Morgengry/Book.cs
@@ -10,19 +10,16 @@ namespace Morgengry {
         public string Title;
         public double Price;
 
-        public Book(string itemId) {
-            ItemID = itemId;
-        }
-
-        public Book(string itemId, string title) {
-            ItemID = itemId;
-            Title = title;
-        }
-
         public Book(string itemId, string title, double price) {
             ItemID = itemId;
             Title = title;
             Price = price;
+        }
+
+        public Book(string itemId) : this(itemId, null, 0) {
+        }
+
+        public Book(string itemId, string title) : this(itemId, title, 0) {
         }
 
         public override string ToString() {
