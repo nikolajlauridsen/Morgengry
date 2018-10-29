f34b2653183944dbdba8cf094a5adadcb253f9f4
Molex <simplesleeptimer@gmail.com>
2018-10-29 09:26:04 +0100

    Implement BookRepository

--- Morgengry/BookRepository.cs
+++ Morgengry/BookRepository.cs
@@ -6,19 +6,30 @@ using System.Threading.Tasks;
 
 namespace Morgengry {
     public class BookRepository {
-        private List<Book> book;
+        private List<Book> books;
 
 
         public void AddBook(Book book) {
-            throw new NotImplementedException();
+            books.Add(book);
         }
 
-        public Book GetBoook(int itemId) {
-            throw new NotImplementedException();
+        public Book GetBoook(string itemId) {
+            foreach(Book book in books) {
+                if(book.ItemId.Equals(itemId)) {
+                    return book;
+                }
+            }
+            return null;
         }
 
         public double GetTotalValue() {
-            throw new NotImplementedException();
+            double value = 0;
+
+            foreach(Book book in books) {
+                value += book.Price;
+            }
+
+            return value;
         }
 
     }
