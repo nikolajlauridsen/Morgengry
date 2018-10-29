aa6d373da8a0cd75af7fe6f81ba8485d1e543663
Molex <simplesleeptimer@gmail.com>
2018-10-29 08:26:17 +0100

    Add test and class frame

---
+++ Morgengry/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry {
+    public class Utility {
+        public static double GetValueOfBook(Book book) {
+            throw new NotImplementedException();
+        }
+
+        public static double GetValueOfAmulet(Amulet amulet) {
+            throw new NotImplementedException();
+        }
+
+        public static double GetValueOfCourse(Book book) {
+            throw new NotImplementedException();
+        }
+    }
+}
