a90ca933af749824e7619fd30712e5003f972905
Molex <simplesleeptimer@gmail.com>
2018-10-29 08:57:10 +0100

    Implement ValueOfAmulet and ValueOfCourse

--- Morgengry/Utility.cs
+++ Morgengry/Utility.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Morgengry;
 
 namespace Morgengry {
     public class Utility {
         public static double GetValueOfBook(Book book) {
-            throw new NotImplementedException();
+            return book.Price;
         }
 
         public static double GetValueOfAmulet(Amulet amulet) {
-            throw new NotImplementedException();
+            switch (amulet.Quality) {
+                case Level.high:
+                    return 27.5;
+                case Level.medium:
+                    return 20.0;
+                default:
+                    return 12.5;
+            }
         }
 
-        public static double GetValueOfCourse(Book book) {
-            throw new NotImplementedException();
+        public static double GetValueOfCourse(Course course) { 
+            int wholeHours = (int) course.DurationInMinutes / 60;
+            double price = 875 * wholeHours;
+            if((course.DurationInMinutes % 60) != 0) {
+                price += 875;
+            }
+            return price;
         }
     }
 }
